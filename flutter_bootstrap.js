49d3975290ddb1025710f44934a43b0aea749294
teddywang0824 <130910538+teddywang0824@users.noreply.github.com>
2025-02-14 16:24:54 +0000

    Deploy Version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1563267758"
+    serviceWorkerVersion: "2883503903"
   }
 });
